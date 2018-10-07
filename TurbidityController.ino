957f0dec4d628c65633494786ac05c9eb9d8a280
TS22082 <ts22082@gmail.com>
2018-10-07 16:23:16 -0700

    set constraints to make sure pump is never off

--- TurbidityController.ino
+++ TurbidityController.ino
@@ -1,31 +1,36 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
-   
+
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
-int motorPin = 9; //ENA on H-BRIDGE (PWM PIN)
-int in1 = 2; // pins to control polarity on h-bridge
-int in2 = 3; // pins to control polarity on h-bridge
-int motorValue = 0;
+int pumpPin = 9; //ENA on H-BRIDGE (PWM PIN)
+int in1 = 2;     // in1 and in2 pins control polarity on h-bridge
+int in2 = 3;
+int pumpSpeedValue = 0;
 int luxValue;
 
-void configureSensor(void) {
-  tsl.enableAutoRange(true); /* Auto-gain ... switches automatically between 1x and 16x */
-  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+void configureSensor(void)
+{
+  tsl.enableAutoRange(true);                            /* Auto-gain ... switches automatically between 1x and 16x */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS); /* fast but low resolution */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
 }
 
-void setup(void) {
+void setup(void)
+{
   Serial.begin(9600);
-  pinMode(motorPin, OUTPUT);
+  pinMode(pumpPin, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  Serial.println("Light Sensor Test"); Serial.println("");  
-  if(!tsl.begin()) {
+  Serial.println("Light Sensor Test");
+  Serial.println("");
+  if (!tsl.begin())
+  {
     Serial.print("No TSL2561 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    while (1)
+      ;
   }
   configureSensor();
   Serial.println("");
@@ -33,22 +38,24 @@ void setup(void) {
   digitalWrite(in2, HIGH);
 }
 
-void loop(void) {  
+void loop(void)
+{
   sensors_event_t event;
   tsl.getEvent(&event);
 
-  if (event.light){
-    luxValue = event.light;  
-    motorValue = map(luxValue, 0, 1023, 0, 255) - 255;
-    analogWrite(motorPin, motorValue);  
-    Serial.print("Lux Value = ");     
-    Serial.print(luxValue);
-    Serial.print(" motor = ");
-    Serial.println(motorValue);
+  if (event.light)
+  {
+    luxValue = event.light;
+    pumpSpeedValue = 255 - map(luxValue, 0, 1023, 0, 255);
+    constrain(pumpSpeedValue, 100, 255); //constraint to make sure motor is never off
+    analogWrite(pumpPin, pumpSpeedValue);
+    Serial.println("Lux Value: " + luxValue);
+    Serial.println("pump speed (0-255): " + pumpSpeedValue);
     delay(2);
   }
-  else {
+  else
+  {
     Serial.println("Sensor overload");
   }
-  delay(250);    
+  delay(250);
 }
